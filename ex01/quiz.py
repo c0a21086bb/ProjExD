6111a1346617f84b3a41ddd92e7531de2a912649
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-14 16:26:59 +0900

    こぴぺ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,24 +1,25 @@
-#未完成
+import random
+
 def main():
     seikai = shutudai()
     kaitou(seikai)
 
 
 def shutudai():
-    qas = ["サザエの旦那の名前は?", 
-                "カツオの妹の名前は?",
-                "タラオはカツオから見てどんな関係?"]
-    answer = [["マスオ", "ますお"],
-              ["ワカメ", "わかめ"],
-              ["甥", "おい", "甥っ子", "おいっこ"]]
-    import random
-    question_number = (randint(0,2))
-    print(question(question_number))
+    qas = [
+           {"q": "カツオの旦那の名前は？", "a": ["マスオ", "ますお"]},
+           {"q": "カツオの妹の名前は？", "a": ["ワカメ", "わかめ"]},
+           {"q": "タラオはカツオから見てどんな関係？", "a": ["甥", "おい", "甥っ子", "おいっこ"]},
+           ]
+    print("問題 : ")
+    r = random.randint(0,2)
+    print(qas[r]["q"])
+    return qas[r]["a"]
 
 
-def kaito():
-    player_answer = input("答えるんだ : ")
-    if player_answer == answer[question_number]:
+def kaitou(seikai):
+    ans = input("答えるんだ : ")
+    if ans in seikai:
         print("正解！！！")
     else:
         print("出直してこい")
