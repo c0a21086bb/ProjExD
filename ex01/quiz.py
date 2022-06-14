af02fd1ad7c56679663ce3ade6ba6e69425d1a50
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-14 16:40:19 +0900

    完成版(コピペ) #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,13 +1,16 @@
 import random
+import datetime
 
 def main():
+    st = datetime.datetime.now()
     seikai = shutudai()
     kaitou(seikai)
-
+    ed = datetime.datetime.now()
+    print("回答時間"+str(((ed-st).seconds))+"秒")
 
 def shutudai():
     qas = [
-           {"q": "カツオの旦那の名前は？", "a": ["マスオ", "ますお"]},
+           {"q": "サザエの旦那の名前は？", "a": ["マスオ", "ますお"]},
            {"q": "カツオの妹の名前は？", "a": ["ワカメ", "わかめ"]},
            {"q": "タラオはカツオから見てどんな関係？", "a": ["甥", "おい", "甥っ子", "おいっこ"]},
            ]
@@ -16,14 +19,14 @@ def shutudai():
     print(qas[r]["q"])
     return qas[r]["a"]
 
-
 def kaitou(seikai):
     ans = input("答えるんだ : ")
     if ans in seikai:
         print("正解！！！")
+    if ans == "33-4":
+        print("何でや！阪神関係ないやろ！")
     else:
         print("出直してこい")
-
 
 if __name__ == "__main__":
     main()
