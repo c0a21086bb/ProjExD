1c8c81bdb6ef7985d17b81c05c197b6de377c0f6
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-21 14:54:25 +0900

    電卓 6問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,12 +10,13 @@ def button_click(event):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("300x500")
+    #root.geometry("300x500")
     root.title("電卓")
 
-    entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
+    entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))]
+    entry.grid(row = 0, colmn = 0, columnspan = 3)
     r, c = 0, 0
-    for num in range (9, -1, -1):
+    for i, num in enumerate ([i for i in range(9, -1, -1)] + ["+"]):
         btn = tk.Button(root,
                         text=f"{num}",
                         width=4,
