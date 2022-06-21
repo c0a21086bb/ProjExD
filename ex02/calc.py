8b27af31a8c92ba7fcfe4d1308173166cbf773b3
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-21 14:36:28 +0900

    電卓 3問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,12 +1,30 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+
+def button_click(event):
+    btn = event.widget
+    num = btn["txt"]
+    tkm.showinfo("", f"{num}のボタンがクリックされました。")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x500")
     root.title("電卓")
 
-    btn = tk.button(root,
-                    text = 9,
-                    )
+    r, c = 0, 0
+    for num in range (9, -1, -1):
+        btn = tk.Button(root,
+                        text=f"{num}",
+                        width=4,
+                        height=2,
+                        font=("times New Roman", 30)
+                        )
+        btn.grid(row = r, column = c)
+        c += 1
+        if (num-1) % 3 == 0:
+            r += 1
+            c = 0
+
 
     root.mainloop
