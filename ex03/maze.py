9bdfe3ca632227fb53371accd06c5e2468cf00f4
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-28 15:14:42 +0900

    問7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -9,9 +10,21 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy
+    delta = {#キー：押されているキーkey/値：移動幅リスト[x, y]
+        ""     :[0, 0],
+        "Up"   :[0, -20],
+        "Down" :[0, +20],
+        "Left" :[-20, 0],
+        "Right":[+20, 0]}
+    cx, cy = cx+delta[key][0], cy+delta[key][1]
+    canvas.coords("kusodori", cx, cy)
+    root.after(50, main_proc)
+
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷える糞鳥")
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
@@ -24,4 +37,5 @@ if __name__ == "__main__":
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
 
+    main_proc()
     root.mainloop()
