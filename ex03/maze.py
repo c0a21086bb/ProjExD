336aba23eb281d9e8cbd8deba475591dd2e28ba1
C0A21086 <c0a21086bb@edu.teu.ac.jp>
2022-06-28 15:43:22 +0900

    基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,16 +11,22 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     delta = {#キー：押されているキーkey/値：移動幅リスト[x, y]
-        ""     :[0, 0],
-        "Up"   :[0, -20],
-        "Down" :[0, +20],
-        "Left" :[-20, 0],
-        "Right":[+20, 0]}
-    cx, cy = cx+delta[key][0], cy+delta[key][1]
+        ""     :[0,  0],
+        "Up"   :[0, -1],
+        "Down" :[0, +1],
+        "Left" :[-1, 0],
+        "Right":[+1, 0],
+    }
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
+            my, mx = my+delta[key][1], mx+delta[key][0]
+    except:
+        pass
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("kusodori", cx, cy)
-    root.after(50, main_proc)
+    root.after(100, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -28,9 +34,12 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+    maze_bg = mm.make_maze(15, 9)
+    mm.show_maze(canvas, maze_bg)
 
     kusodori = tk.PhotoImage(file="fig/0.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=kusodori, tag="kusodori")
 
     key = ""
